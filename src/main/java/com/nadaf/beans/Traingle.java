881b4e2eadfeb98540ffba57f8b7d32b4d20b29f
ElsayedAwdElsayed <elsayedawd25@gmail.com>
2017-03-10 11:13:28 +0200

    Spring life Cycle call backs include two mechanisms (Implementing Spring Interfaces , configure Spring.xml)

--- src/main/java/com/nadaf/beans/Traingle.java
+++ src/main/java/com/nadaf/beans/Traingle.java
@@ -9,6 +9,8 @@ import java.io.Serializable;
 import java.util.List;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
@@ -16,8 +18,10 @@ import org.springframework.context.ApplicationContextAware;
  *
  * @author elsayedawd
  */
-
-public class Traingle implements Serializable,ApplicationContextAware,BeanNameAware{
+//Implement Intializing bean ===> after setting the properites in the Spring.xml afterProperties method will be 
+//called 
+//Implementing DisposableBean when the application is closed and beans is cleaned  this method will be called
+public class Traingle implements Serializable,ApplicationContextAware,BeanNameAware,InitializingBean,DisposableBean{
   
  private List<Point> points;
  
@@ -43,15 +47,36 @@ public class Traingle implements Serializable,ApplicationContextAware,BeanNameAw
         System.out.println("draw method called here ----------------------"+ points.size());
         
     }
-
+ //it is called after setting the application context that we have 
     @Override
     public void setApplicationContext(ApplicationContext context) throws BeansException {
         System.out.println("application context -----------------");
     }
-
+//it is called first time 
     @Override
     public void setBeanName(String name) {
         System.out.println(name);
+    }
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Intializing Bean ------------------ ");
+    }
+    
+  //This method is called when the application is closed and the beaned is cleaning up 
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("cleaning Up -------------------------- ");          
+    }
+    //if I want to not to implements the the IntializingBean and the DisposableBean you should make this method and
+    //then add this to the Spring.xml Configration 
+    public void myInit()
+    {
+        System.out.println("MyInit Method is called ----------- ");
+    }
+    //also if I need to make my destroy method 
+    public void myDestroy()
+    {
+        System.out.println("Mydestroy is called -------------- ");
     }
    
    
--- src/main/java/com/nadaf/main/Main.java
+++ src/main/java/com/nadaf/main/Main.java
@@ -9,6 +9,7 @@ import com.nadaf.beans.Traingle;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
@@ -20,7 +21,10 @@ public class Main {
 
     public static void main(String[] args) {
         //BeanFactory factory=new XmlBeanFactory(new FileSystemResource("spring.xml"));
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        //ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); the Normal application Context
+        //the abstract application context that is used in the make the intialization events and destruction events of Spring beans
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        context.registerShutdownHook(); //when the application close the context closes and destroyes all the beans 
         Traingle traingle1 = (Traingle) context.getBean("traingle1");
         traingle1.draw();
     }
